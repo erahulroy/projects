0e39501e5f8231a184a9b6ddcac9cebd926de4be
Rahul Roy <erahulroy123@gmail.com>
2026-01-03 02:16:48 +0530

    Update main.py

--- weather/main.py
+++ weather/main.py
@@ -12,8 +12,14 @@ def get_weather(city):
     }
 
     try:
+        # fetched_data stores http response object like:
+        #   fetched_data.status_code: 200 if data found, 404 if no city found, 401 if invalid API key
+        #   fetched_data.text: received data in text formate (here we receive in JSON (plain text formated like dictionaries) formate)
+        # if there is no internet connection then it raises requests.exceptions.ConnectionError
         fetched_data = requests.get(base_url, params=params)
+        # .raise_for_status() method is used to check http status code. if code is 200-299, successful fetch and does nothing. if code is 400-499, means client error and raises requests.exceptions.HTTPError. if code is 500-599, means server error and raises requests.exceptions.HTTPError.
         fetched_data.raise_for_status()
+        # .json() method used to convert received JSON to python dictionaries
         weather_data = fetched_data.json()
 
         # Extract all available weather information
